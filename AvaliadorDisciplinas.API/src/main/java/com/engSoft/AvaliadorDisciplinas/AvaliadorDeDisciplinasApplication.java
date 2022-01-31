ccd66f9cf05b3822dd8db5cb26753067e6cd16fb
Holliver <holliver.costa@ccc.ufcg.edu.br>
2022-01-31 17:10:35 -0300

    base do repositorio

---
+++ AvaliadorDisciplinas.API/src/main/java/com/engSoft/AvaliadorDisciplinas/AvaliadorDeDisciplinasApplication.java
@@ -0,0 +1,13 @@
+package com.engSoft.AvaliadorDisciplinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliadorDeDisciplinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliadorDeDisciplinasApplication.class, args);
+	}
+
+}
---
+++ AvaliadorDisciplinas.API/src/test/java/com/engSoft/AvaliadorDisciplinas/AvaliadorDeDisciplinasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engSoft.AvaliadorDisciplinas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliadorDeDisciplinasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
