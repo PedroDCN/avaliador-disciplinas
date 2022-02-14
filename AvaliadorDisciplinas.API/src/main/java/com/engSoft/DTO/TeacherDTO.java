515adf4ab9a8a053df7f73ea2b4f15cd72b7a1b3
Lucas Brasileiro <lucas.raposo@ccc.ufcg.edu.br>
2022-02-14 12:12:34 -0300

    creating TeacherDTO

---
+++ AvaliadorDisciplinas.API/src/main/java/com/engSoft/DTO/TeacherDTO.java
@@ -0,0 +1,17 @@
+package com.engSoft.DTO;
+
+public class TeacherDTO {
+    private String name;
+
+    public TeacherDTO(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
