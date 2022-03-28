1f8017abc715cf07d38b42b8abd1e1c560f2bc30
MelquiSilva <melquisedeque.silva@ccc.ufcg.edu.br>
2022-03-28 12:31:01 -0300

    adicionando patch do nickName e tela perfil do usuario consumindo api

--- AvaliadorDisciplinas.FRONTEND/src/pages/UserPage/index.jsx
+++ AvaliadorDisciplinas.FRONTEND/src/pages/UserPage/index.jsx
@@ -5,12 +5,43 @@ import { useAuth } from '../../contexts/AuthContext';
 import UserImage from '../../assets/icons/user_anonimous.svg';
 import styles from './UserPage.module.css';
 import { useNavigate } from 'react-router-dom';
-
+import { getUser, updateNickUser } from '../../services/userAvaliacoesService';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function UserPage() {
     const { user } = useAuth();
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
+    const [student, setEstudent] = useState([]);
+    const [nick, setNick] = useState([]);
+
+    const notifySucess = (message) => toast.success(message);
+    const notifyFailure = (message) => toast.error(message);
+
+    useEffect(() => {
+        async function fetchData() {
+            if (user) {
+                setLoading(true);
+                const data = (await getUser(user.id)).data;
+
+                setEstudent(data);
+                setNick(data.nick);
+                setLoading(false);
+
+            }
+        }
+        fetchData();
+    }, [user]);
+
+    function handleNickButton() {
+        updateNickUser(student.email, nick).then(() => {
+            notifySucess("NickName editado com sucesso!")
+        }).catch(e => {
+            notifyFailure("Erro na edição");
+        });
+    }
     return (
         <div className={styles.container}>
             <div className={styles.content}>
@@ -31,15 +62,16 @@ function UserPage() {
                             src={user === undefined ? UserImage : user.photo}
                             alt="User Logged"
                             height={96}
-                            width={96} 
+                            width={96}
                             referrerPolicy="no-referrer"
                         />
                     </div>
 
                     <div className={styles.inputs}>
-                        <input type="email" placeholder='email@ccc.ufcg.edu.br' disabled />
-                        <input type="userName" placeholder='Name Surname' disabled />
-                        <input type="nickName" placeholder='nickname' />
+                        <input type="email" placeholder={student ? student.email : 'email@ccc.ufcg.edu.br'} disabled />
+                        <input type="userName" placeholder={student ? student.name : 'Name Surname'} disabled />
+                        <input type="nickName" placeholder={student ? student.nick : 'nickname'}
+                            onChange={(e) => { setNick(e.target.value) }} />
                     </div>
                     <div className={styles.buttons}>
 
@@ -48,7 +80,19 @@ function UserPage() {
                             backgroundcolor={colors.theme.secondary}
                             width="14rem"
                             color={colors.theme.white}
-                        // onClick={}
+                            onClick={handleNickButton}
+                        />
+                        <ToastContainer
+                            position="top-right"
+                            autoClose={1500}
+                            hideProgressBar={false}
+                            closeButton={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover={false}
                         />
                     </div>
                 </div>
--- AvaliadorDisciplinas.FRONTEND/src/services/userAvaliacoesService.js
+++ AvaliadorDisciplinas.FRONTEND/src/services/userAvaliacoesService.js
@@ -4,4 +4,12 @@ async function getAllUser(id) {
     return await api.get(`/feedback/listByUser/${id}`);
 }
 
-export { getAllUser };
+async function getUser(id) {
+    return await api.get(`/users/${id}`);
+}
+
+async function updateNickUser(email, nick) {
+    return await api.patch(`user/${email}?nick=${nick}`);
+
+}
+export { getAllUser, getUser, updateNickUser };
