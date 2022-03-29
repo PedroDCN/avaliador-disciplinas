a946ab70c7a25704de647be63df6320b62152357
Juhrer <lucas.vigolvino@ccc.ufcg.edu.br>
2022-03-29 20:58:04 -0300

    FeedbackController com os retornos corrigidos

--- AvaliadorDisciplinas.API/src/main/java/com/engSoft/controllers/FeedbackController.java
+++ AvaliadorDisciplinas.API/src/main/java/com/engSoft/controllers/FeedbackController.java
@@ -1,6 +1,7 @@
 package com.engSoft.controllers;
 
 import com.engSoft.DTO.FeedbackDTO;
+import com.engSoft.DTO.ReturnFeedbackDTO;
 import com.engSoft.entities.*;
 import com.engSoft.services.CourseService;
 import com.engSoft.services.FeedbackService;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 @RestController
@@ -60,7 +62,7 @@ public class FeedbackController {
         try {
             feedbackService.saveFeedback(newFeedback);
             courseService.updateGrade(optionalCourse.get(), feedbackService.listFeedbackByCourse(optionalCourse.get().getId()));
-            return new ResponseEntity<>(newFeedback, HttpStatus.CREATED);
+            return new ResponseEntity<>(new ReturnFeedbackDTO(newFeedback), HttpStatus.CREATED);
         }catch (Error e){
             return new ResponseEntity<>(
                      new CustomErrorType("Error, feedback can´t be created"), HttpStatus.BAD_REQUEST);
@@ -70,7 +72,7 @@ public class FeedbackController {
     @RequestMapping(value = "/feedback", method = RequestMethod.GET)
     public ResponseEntity<?> getAllFeedback(){
         List<Feedback> feedbacks = this.feedbackService.listFeedbacks();
-        return new ResponseEntity<>(feedbacks, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(toListReturnFeedbackDTO(feedbacks), HttpStatus.ACCEPTED);
     }
 
     @RequestMapping(value = "/feedback/listByUser/{idUser}", method = RequestMethod.GET)
@@ -82,7 +84,7 @@ public class FeedbackController {
             return ErroUser.erroUserNotFound();
         }
         List<Feedback> feedbacks = feedbackService.findFeedbackByStudent(idUser);
-        return new ResponseEntity<>(feedbacks, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(toListReturnFeedbackDTO(feedbacks), HttpStatus.ACCEPTED);
 
     }
 
@@ -94,7 +96,7 @@ public class FeedbackController {
             return ErroCourse.erroCourseNotFound();
         }
         List<Feedback> feedbacks = feedbackService.listFeedbackByCourse(idCourse);
-        return new ResponseEntity<>(feedbacks, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(toListReturnFeedbackDTO(feedbacks), HttpStatus.ACCEPTED);
 
     }
 
@@ -119,7 +121,7 @@ public class FeedbackController {
             return ErroSemester.erroSemesterNotFound();
         }
         List<Feedback> feedbacks = feedbackService.findFeedbackBySemester(idSemester);
-        return new ResponseEntity<>(feedbacks, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(toListReturnFeedbackDTO(feedbacks), HttpStatus.ACCEPTED);
 
     }
 
@@ -135,7 +137,7 @@ public class FeedbackController {
             return ErroCourse.erroCourseNotFound();
         }
         List<Feedback> feedbacks = feedbackService.findFeedbackByCourseAndSemester(idCourse, idSemester);
-        return new ResponseEntity<>(feedbacks, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(toListReturnFeedbackDTO(feedbacks), HttpStatus.ACCEPTED);
 
     }
 
@@ -163,7 +165,7 @@ public class FeedbackController {
         if(!optionalFeedback.isPresent())
             return ErroFeedback.erroFeedbackNotFound();
 
-        return new ResponseEntity<>(optionalFeedback, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(new ReturnFeedbackDTO(optionalFeedback.get()), HttpStatus.ACCEPTED);
     }
 
     @RequestMapping(value = "/feedback/{id}", method = RequestMethod.DELETE)
@@ -177,11 +179,18 @@ public class FeedbackController {
             feedbackService.removeFeedback(id);
             Course course = optionalFeedback.get().getCourse();
             courseService.updateGrade(course, feedbackService.listFeedbackByCourse(course.getId()));
-            return new ResponseEntity<>(optionalFeedback, HttpStatus.OK);
+            return new ResponseEntity<>(new ReturnFeedbackDTO(optionalFeedback.get()), HttpStatus.OK);
         }catch (Error e ){
             return new ResponseEntity<>(
                     new CustomErrorType("Error, feedback can´t be deleted"), HttpStatus.BAD_REQUEST);
         }
+    }
 
+    private List<ReturnFeedbackDTO> toListReturnFeedbackDTO(List<Feedback> list) {
+        List<ReturnFeedbackDTO> returnList = new ArrayList<>();
+        for(Feedback feedback : list) {
+            returnList.add(new ReturnFeedbackDTO(feedback));
+        }
+        return returnList;
     }
 }
