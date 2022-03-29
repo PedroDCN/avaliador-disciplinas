cf27c80cf68ffd34572c75c5d79ddff0bb6063c4
Rodrigo <rodrigotmeira@hotmail.com>
2022-03-29 09:51:52 -0300

    criar comentario correto

--- src/main/java/com/engSoft/controllers/CommentController.java
+++ src/main/java/com/engSoft/controllers/CommentController.java
@@ -46,7 +46,7 @@ public class CommentController {
         if (!optionalStudent.isPresent()){
             return ErroUser.erroUserNotFound();
         }
-        Comment newComment = new Comment(commentDTO, optionalStudent.get().getNick(), optionalStudent.get().getPhoto_url());
+        Comment newComment = new Comment(commentDTO, optionalCourse.get().getName(), optionalStudent.get().getNick(), optionalStudent.get().getPhoto_url());
         try {
             commentService.saveComment(newComment);
             return new ResponseEntity<>(newComment, HttpStatus.CREATED);
